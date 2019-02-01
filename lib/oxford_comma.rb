090f289755602150cd56d44f09044a1d90fc851c
Jason Fournier <jmf4nier@hotmail.com>
2019-02-01 18:32:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
+array = []
 def oxford_comma(array)
-
+ if array.size > 2                    #sorts by size of array. only continues if size > 2
+  oxford = array.pop                  #pulls last element from array        
+  last_plus_and = "and " + oxford      #adds "and" to the .pop return
+  array.push(last_plus_and)            #pushes "and + .pop" to the end of the array
+  string = array.join(", ")
+  p string
+ elsif array.size == 2                  #if only 2 elements in array, just adds "and" between them
+  string = array.join(" and ")
+  p string
+ else                                   #if only 1 element, does nothing but convert to string
+  p array.join
+ end    
+  
 end
+
+ 
+ 
+ 
+ 
