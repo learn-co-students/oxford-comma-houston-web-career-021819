54ff1f243e36d34664568bc99579a9c7f6f65650
zepher2211 <zepher2211@gmail.com>
2019-02-18 20:52:37 -0600

    oxford comma'd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ if array.length == 1
+ "#{array[0]}"
+elsif array.length == 2
+ "#{array[0]} and #{array[1]}"
+elsif array.length == 3
+ "#{array[0]}, #{array[1]}, and #{array[2]}"
+else
+  last = array.last.to_s
+  first = "and "
+  first << last
+  array.pop
+  array << first
+  array.join(", ")
+end
 end
